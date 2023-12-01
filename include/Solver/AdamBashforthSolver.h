1c0f48c26fb56fad2c99e06f9608790185400e06
Matthias Forestier <forestie@ma-linux23-08.intranet.epfl.ch>
2023-12-01 15:47:13 +0100

    ABv1

--- include/Solver/AdamBashforthSolver.h
+++ include/Solver/AdamBashforthSolver.h
@@ -5,12 +5,8 @@
 #include <vector>
 #include "DiscreteSolver.hh"
 
-class AdamBashforthSolver : public DiscreteSolver<double> {
+class AdamBashforthSolver : public DiscreteSolver {
 public:
-    // Constructor without initialMethod
-    AdamBashforthSolver(double (*function)(double, double), double initialY, double deltaTime, int steps)
-        : DiscreteSolver<double>(function, initialY, deltaTime, steps) {}
-
     virtual std::pair<std::vector<double>, std::vector<double>> solve() const override;
 };
 
