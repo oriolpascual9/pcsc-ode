f1f25ff439dbed0344fc5cefa42d05dd0ee36394
Matthias Forestier <forestie@ma-linux23-08.intranet.epfl.ch>
2023-12-01 16:51:50 +0100

    functionv1

--- src/FunctionComponent/FunctionComponent.cpp
+++ src/FunctionComponent/FunctionComponent.cpp
@@ -23,3 +23,15 @@ void FunctionComponent::generateExpComponent(double base) {
 DoubleFunction FunctionComponent::getFunction() {
     return functionComponent;
 }
+
+FunctionComponent FunctionComponent::sum(const FunctionComponent& other) const {
+  FunctionComponent result;
+  DoubleFunction f1 = this->getFunction();
+  DoubleFunction f2 = other.getFunction();
+  
+  result.functionComponent = [f1, f2](double* t, double* y) {
+    return f1(t, y) + f2(t, y);
+  };
+  
+  return result;
+}
--- src/Reader/Reader.cpp
+++ src/Reader/Reader.cpp
@@ -1,5 +1,9 @@
 #include "Reader/Reader.hh"
-
+#include "FunctionComponent/FunctionComponent.hh"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <regex>
 /* -------------------------------------------------------------------------- */
 
 Problem& Reader::read_problem(){
@@ -29,22 +33,64 @@ int Reader::read_type_of_problem() {
 
 /* --------------------------------------------------------------------------- */
 
-Function Reader::read_function(){
-    char instructions;
-    std::string functionStr;
-    std::cout << "Now introduce the function, do you want to know the instructions? (y/n)";
-    std::cin >> instructions;
 
-    if (instructions == 'y') {
-        // TODO: Come up with an easy way to print the instructions
-        std::cout << "This is supposed to be the instructions\n";
+
+Function Reader::read_function() {
+  char instructions;
+  std::string functionStr;
+  
+  std::cout << "Now introduce the function, do you want to know the instructions? (y/n): ";
+  std::cin >> instructions;
+  
+  if (instructions == 'y' || instructions == 'Y') {
+    std::cout << "Instructions:\n";
+    std::cout << "Enter the function in the format: a*x^b + c*x^d + ...\n";
+    std::cout << "For example, '3*x^2 - 1.5*x + 4'\n";
+  }
+  
+  std::cout << "Enter your function: ";
+  std::cin.ignore(); // Ignore leftover newline from previous input
+  std::getline(std::cin, functionStr);
+  
+  return parse_function(functionStr);
+}
+
+Function Reader::parse_function_poly(const std::string& input) {
+  Function function;
+  std::istringstream iss(input);
+  std::string token;
+  
+  // Regular expression for matching polynomial terms like "3*x^2"
+  std::regex termPattern(R"(([-+]?[0-9]*\.?[0-9]+)\*x\^([-+]?[0-9]*\.?[0-9]+))");
+  
+  while (iss >> token) {
+    if (token == "+" || token == "-" || token == "*" || token == "/") {
+      function.addOperator(token[0]);
+    } else {
+      FunctionComponent component;
+      
+      // Check if the token is a scalar
+      try {
+        double scalar = std::stod(token);
+        component.generateScalarComponent(scalar);
+      } catch (const std::invalid_argument&) {
+        // Not a scalar, check if it is a term
+        std::smatch matches;
+        if (std::regex_match(token, matches, termPattern) && matches.size() == 3) {
+          double scalar = std::stod(matches[1]);
+          double exponent = std::stod(matches[2]);
+          component.generateTermComponent(scalar, exponent);
+        } else {
+          // Handle other types of components or throw an error
+          std::cerr << "Error: Unrecognized component format '" << token << "'\n";
+        }
+      }
+      
+      function.addComponent(component);
     }
-
-    // TODO: Readin function method
-    // TODO: Read Function Component one by one and append them to Function class
-
-    Function function;
-    return function;
+  }
+  
+  return function;
 }
 
 /* --------------------------------------------------------------------------- */
